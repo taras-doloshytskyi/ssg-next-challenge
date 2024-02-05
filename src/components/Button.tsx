a70bc90f1ab5de391296921b5c1883c84cf5c83a
Taras Doloshytskyi <taras.doloshytskyi.work@gmail.com>
2024-02-05 20:46:00 +0100

    feat: added fetching functionality to the buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,24 @@
 import { FunctionComponent, PropsWithChildren } from "react";
 import classNames from "classnames";
 
-type ButtonProps = {};
+type ButtonProps = {
+  onClick: () => void;
+  disabled: boolean;
+};
 
 export const Button: FunctionComponent<PropsWithChildren<ButtonProps>> = ({
   children,
+  onClick,
+  disabled,
 }) => {
   return (
     <button
       type="button"
-      className={classNames("px-2 py-1 border border-black")}
+      className={classNames(
+        `px-2 py-1 border border-black ${disabled && "opacity-50"}`,
+      )}
+      onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
